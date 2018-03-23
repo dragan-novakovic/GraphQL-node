3f080bab943698628e07b9dcfdb4c23c94b4423a
Dragan <d.novakovic1810@yahoo.com>
2018-03-23 09:26:39 +0100

    bump

--- server/server.js
+++ server/server.js
@@ -3,6 +3,6 @@ const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const app = express();
 
-app.use("/graphql", graphqlHTTP({ schema }));
+app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
 app.listen("3333", () => console.log("Started Server on Port 3333 !!!"));
