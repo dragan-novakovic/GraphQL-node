00f7387b69d7b87ef4e6e4124357ae8d1a469a7b
Dragan <d.novakovic1810@yahoo.com>
2018-03-28 13:04:58 +0200

    qq

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const schema = require("./schema/schema");
 const app = express();
 
 // apollo
+// 25.03.2018
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
 app.listen("3333", () => console.log("Started Server on Port 3333 !!!!!"));
