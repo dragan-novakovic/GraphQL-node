2d9e6faf722a23881d99d111179e8d678a4f196c
Dragan <d.novakovic1810@yahoo.com>
2018-04-07 13:54:12 +0200

    react

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import BookList from "./components/BookList";
 
 class App extends Component {
   render() {
     return (
       <>
         <h1>Hii</h1>
+        <BookList />
       </>
     );
   }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class BookList extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>fjwijfiwjf</li>
+        </ul>
+      </div>
+    );
+  }
+}
