f759483f040f5a313d797bf7ccf9e5de6a84dcec
Dragan <d.novakovic1810@yahoo.com>
2018-04-22 15:26:42 +0200

    29 pre fix

--- client/src/App.js
+++ client/src/App.js
@@ -11,13 +11,13 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div>
-          <h1>Hii</h1>
+        <div id="main">
+          <h1>Ninja's Reading List</h1>
           <BookList />
+          <AddBook />
         </div>
       </ApolloProvider>
     );
   }
 }
-
 export default App;
