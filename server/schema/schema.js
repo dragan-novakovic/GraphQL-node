8ecce831a609abd42ea6dd52b267ff21bcafd5eb
Dragan <d.novakovic1810@yahoo.com>
2018-04-24 21:07:09 +0200

    qq

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,5 @@
 const graphql = require("graphql");
+const { makeExecutableSchema } = require('graphql-tools');
 const Book = require("../models/book");
 const Author = require("../models/Author");
 
@@ -11,6 +12,35 @@ const {
   GraphQLList,
   GraphQLNonNull
 } = graphql;
+
+// Some fake data
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query { books: [Book] }
+  type Book { title: String, author: String }
+`;
+
+// The resolvers
+const resolvers = {
+  Query: { books: () => books },
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
 
 const BookType = new GraphQLObjectType({
   name: "Book",
