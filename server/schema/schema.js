96941f3e63abe7d7cccba9fa310c91b43dd2b723
Dragan <d.novakovic1810@yahoo.com>
2018-07-04 12:47:41 +0200

    update to apollo-server 2

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,35 +1,33 @@
-const { makeExecutableSchema } = require("graphql-tools");
+const { gql } = require("apollo-server");
 const { resolvers } = require("./resolvers");
 
-const typeDefs = `
-type Book {
+const typeDefs = gql`
+  type Book {
     id: ID
     name: String
     genre: String
     author: Author
-   }
+  }
   type Author {
-     id: ID
-     name: String
-     age: Int
-     books:[Book]
-   }
+    id: ID
+    name: String
+    age: Int
+    books: [Book]
+  }
   type Query {
     book(id: ID!): Book
     author(id: ID!): Author
-    books:[Book]
-    authors:[Author]
-   }
+    books: [Book]
+    authors: [Author]
+  }
 
   type Mutation {
-    addAuthor(name: String!, age: Int!):Author
-    addBook(name: String!, genre: String!, authorId: ID!):Book
+    addAuthor(name: String!, age: Int!): Author
+    addBook(name: String!, genre: String!, authorId: ID!): Book
   }
 `;
 
 module.exports = {
-  schema: makeExecutableSchema({
-    typeDefs,
-    resolvers
-  })
+  typeDefs,
+  resolvers
 };
--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
-const { schema } = require("./schema/schema");
+const { ApolloServer } = require("apollo-server");
+const { typeDefs, resolvers } = require("./schema/schema");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
@@ -14,7 +13,7 @@ const URL = "mongodb://Ja:ja@ds227939.mlab.com:27939/graphql-db";
 mongoose.connect(URL);
 mongoose.connection.once("open", () => console.log("Connected to DB"));
 
-app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
-app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
 
 app.listen("3333", () => console.log("Started Server on Port 3333 !!!!!"));
